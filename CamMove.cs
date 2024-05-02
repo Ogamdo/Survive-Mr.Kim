a439b90174f9c5e078af2a700970b300b2785e25
hojunhojun <85822956+hojunhojun@users.noreply.github.com>
2024-05-02 13:55:20 +0900

    Add files via upload

---
+++ CamMove.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMove : MonoBehaviour
+{
+    public Transform player;
+    Vector3 offset;
+    void Start()
+    {
+        offset = player.position - transform.position;
+    }
+    void LateUpdate()
+    {
+        transform.position = player.position - offset;
+        transform.LookAt(player);
+    }
+}
