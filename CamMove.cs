dc41f481d77a0048bc8614b0d9fbafaef842cc50
hojunhojun <85822956+hojunhojun@users.noreply.github.com>
2024-05-02 13:53:38 +0900

    Add files via upload

---
+++ CamMove.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMove : MonoBehaviour
+{
+    public Transform target;
+    float dist = 4.0f;
+    float height = 6.0f;
+    Transform tr;
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+    void LateUpdate()
+    {
+        float yAngle = Mathf.LerpAngle(tr.eulerAngles.y, target.eulerAngles.y, Time.deltaTime);
+        Quaternion rot = Quaternion.Euler(0, yAngle, 0);
+        tr.position = target.position - (rot * Vector3.forward * dist) + (Vector3.up * height);
+        tr.LookAt(target);
+    }
+}
